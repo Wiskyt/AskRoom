1ff39367e85c813b78484f95259661d4af281e0c
Wiskyt <samy.bouamama@hotmail.fr>
2017-05-07 07:44:25 +0200

    Avancements

--- public/components/question/question.js
+++ public/components/question/question.js
@@ -3,26 +3,29 @@
 var app = angular.module("askroom");
 
 app.component("question", {
-   templateUrl: "components/question/question.html",
-   controller: Question
+    templateUrl: "components/question/question.html",
+    controller: Question
 });
 
 var question;
 
 function Question($scope, $resource, socket) {
-   var _this = this;
+    var _this = this;
 
-   socket.on("init question", function (question) {
-      swapQuestion(question, _this);
-   });
+    socket.on("init question", function(question) {
+        swapQuestion(question, _this);
+    });
 
-   socket.on("next question", function (nextQuestion) {
-      swapQuestion(nextQuestion, _this);
-   });
+    socket.on("next question", function(nextQuestion) {
+        swapQuestion(nextQuestion, _this);
+    });
 }
 
 function swapQuestion(newQuestion, context) {
-   question = newQuestion;
-   context.author = nextQuestion.author;
-   context.content = nextQuestion.content;
+    question = newQuestion;
+
+    // Effectuer animations et transitions
+
+    context.author = newQuestion.author;
+    context.content = newQuestion.content;
 }
--- server.js
+++ server.js
@@ -11,80 +11,91 @@ var cookieParser = require('cookie-parser');
 var date = new Date();
 
 var typicalQuestion = { id: 0, author: "Wiskyt", content: "Comment devisser un tuyau d'arrosage ?" };
+var typicalQuestion2 = { id: 0, author: "Donzo", content: "Comment on fait pour se frotter le tuyau avec une chaussette ?" };
 var typicalAnswer = { id: 0, questionId: 0, author: "Wiskyt", content: "Va voir sur google" };
-var questions = [];
-// for (let i = 0; i < 5; i++) questions.push(typicalQuestion);
+var questions = [],
+    questionInterval = 5; // In seconds
+
+for (let i = 0; i < 20; i++) {
+    if (i % 2 == 0) {
+        typicalQuestion.id = questions.length;
+        questions.push(typicalQuestion);
+    } else {
+        typicalQuestion2.id = questions.length;
+        questions.push(typicalQuestion2);
+    }
+}
 
 var chatHistory = [],
-   chatHistoryLength = 500,
-   chatId = 0;
+    chatHistoryLength = 500,
+    chatId = 0;
 
 // ---------------------------- SOCKETS
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-io.on('connection', function (socket) { // Lorsqu'une connexion socket est crée on initialie les events
-   console.log("Nouvel user");
+io.on('connection', function(socket) { // Lorsqu'une connexion socket est crée on initialie les events
+    socket.emit("init question", questions[0]);
 
-   socket.on("chat message", function (chatMsg) {
-      if (chatMsg.author && chatMsg.message) {
-         chatMsg.id = chatId;
-         chatId++;
-         chatHistory.push(chatMsg);
-         if (chatHistory.length > chatHistoryLength) chatHistory.shift();
-         io.emit("chat message", chatMsg);
-      }
-   });
+    socket.on("chat message", function(chatMsg) {
+        if (chatMsg.author && chatMsg.message) {
+            chatMsg.id = chatId;
+            chatId++;
+            chatHistory.push(chatMsg);
+            if (chatHistory.length > chatHistoryLength) chatHistory.shift();
+            io.emit("chat message", chatMsg);
+        }
+    });
 
-   socket.on("new question", function (newQuestion) {
-      if (newQuestion.author && newQuestion.content) {
-         newQuestion.date = date.getTime();
-         newQuestion.id = questions.length;
-         io.emit("new question", newQuestion);
+    socket.on("new question", function(newQuestion) {
+        if (newQuestion.author && newQuestion.content) {
+            newQuestion.date = date.getTime();
+            newQuestion.id = questions.length;
+            io.emit("new question", newQuestion);
 
-         newQuestion.answers = [];
-         questions.push(newQuestion);
-      }
-   });
+            newQuestion.answers = [];
+            questions.push(newQuestion);
+        }
+    });
 
-   socket.on("question answer", function (questionAnswer) {
-      if (questionAnswer.hasOwnProperty("questionId") && questionAnswer.author && questionAnswer.content) {
-         let question = getQuestion(questionAnswer.questionId);
-         console.log(question);
-         questionAnswer.id = question.answers.length;
-         question.answers.push(questionAnswer);
-         io.emit("question answer", questionAnswer);
-      }
-   });
+    socket.on("question answer", function(questionAnswer) {
+        if (questionAnswer.hasOwnProperty("questionId") && questionAnswer.author && questionAnswer.content) {
+            let question = getQuestion(questionAnswer.questionId);
+            console.log(question);
+            questionAnswer.id = question.answers.length;
+            question.answers.push(questionAnswer);
+            io.emit("question answer", questionAnswer);
+        }
+    });
 
-   socket.on("answer upvote", function (upvoteInfo) {
-      if (upvoteInfo.hasOwnProperty("questionId") && upvoteInfo.hasOwnProperty("answerId")) {
-         let answer = getAnswer(upvoteInfo.questionId, upvoteInfo.answerId);
-         if (answer) {
-            answer.upvotes++;
-            io.emit("answer upvote", upvoteInfo);
-         }
-      }
-   });
+    socket.on("answer upvote", function(upvoteInfo) {
+        if (upvoteInfo.hasOwnProperty("questionId") && upvoteInfo.hasOwnProperty("answerId")) {
+            let answer = getAnswer(upvoteInfo.questionId, upvoteInfo.answerId);
+            if (answer) {
+                answer.upvotes++;
+                io.emit("answer upvote", upvoteInfo);
+            }
+        }
+    });
 });
 
-setInterval(function () {
-   console.log("Question switch ! State : ", questions);
-   if (questions[0]) {
-      io.emit("next question", questions[0]);
-      var questionSave = questions.shift();
-   }
-}, 10 * 1000); // 60 seconds
+setInterval(function() {
+    console.log("Question switch ! State : ", questions);
+    if (questions[0]) {
+        io.emit("next question", questions[0]);
+        var questionSave = questions.shift();
+    }
+}, questionInterval * 1000); // 60 seconds
 
 server.listen(3000);
 
 function getQuestion(id) {
-   return questions[questions.map(function (q) { return q.id; }).indexOf(id)] || null;
+    return questions[questions.map(function(q) { return q.id; }).indexOf(id)] || null;
 }
 
 function getAnswer(answerId, questionId) {
-   let q = getQuestion(questionId);
-   return q.answers[q.answers.map(function (a) { return a.id; }).indexOf(answerId)] || null;
+    let q = getQuestion(questionId);
+    return q.answers[q.answers.map(function(a) { return a.id; }).indexOf(answerId)] || null;
 }
 
 // FIN SOCKETS ------------------------
@@ -99,9 +110,9 @@ app.use(bodyParser.json());
 
 // Express session
 app.use(session({
-   secret: 'reallysecret',
-   saveUninitialized: true,
-   resave: true
+    secret: 'reallysecret',
+    saveUninitialized: true,
+    resave: true
 }));
 
 // Serving static folder
@@ -112,6 +123,6 @@ app.use(express.static(__dirname + '/node_modules'));
 
 app.use('/', routes);
 
-app.listen(7077, function () {
-   console.log('Listening on port 7077!');
+app.listen(7077, function() {
+    console.log('Listening on port 7077!');
 });
