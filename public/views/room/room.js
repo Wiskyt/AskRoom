e486b6f11a911e6ef3a1740c473979e67fb2b27c
Wiskyt <samy.bouamama@hotmail.fr>
2017-05-07 03:22:07 +0200

    good stuff

--- public/views/room/room.js
+++ public/views/room/room.js
@@ -7,26 +7,41 @@ app.component("room", {
     controller: Room
 });
 
-function Room($scope, $resource) {
+function Room($scope, $resource, socket) {
     var _this = this;
 
     $scope.test = "Yo mon pote";
 
-    var socket = io("http://localhost:3000");
 
     socket.on("connect", function() {
         console.log("lets go");
-        var msg = { author: "Wiskyt", message: "Yo les noobs" };
+        var typicalChat = { author: "Wiskyt", message: "yo les noobs" };
+        var typicalQuestion = { id: 0, author: "Wiskyt", content: "Comment devisser un tuyau d'arrosage ?" };
+        var typicalAnswer = { id: 0, questionId: 0, author: "Wiskyt", content: "Va voir sur google" };
+        var typicalUpvote = { questionId: 0, answerId: 0 };
 
-        // to make things interesting, have it send every second
-        var interval = setInterval(function() {
-            socket.emit("chat message", msg);
-            console.log("on est la");
-        }, 1000);
+        socket.emit("chat message", typicalChat);
+        socket.emit("new question", typicalQuestion);
 
         socket.on("chat message", function(obj) {
-            console.log("Received", obj);
+            console.log("CM Received", obj);
         });
+
+        socket.on("new question", function(obj) {
+            console.log("NQ Received", obj);
+
+            socket.emit("question answer", typicalAnswer);
+        });
+
+        socket.on("question answer", function(obj) {
+            console.log("QA Received", obj);
+            socket.emit("answer upvote", typicalUpvote);
+        });
+
+        socket.on("answer upvote", function(obj) {
+            console.log("AU Received", obj);
+        });
+
 
         socket.on("disconnect", function() {
             clearInterval(interval);
